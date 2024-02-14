e45230f312ef8bb2954e4a464f9f11dce1584b09
MV4Tech <mariyan102@abv.bg>
2024-02-14 17:35:37 +0200

    initializing ract vite

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <div>Home</div>
+        <label>Zdraveite na vsichki kak ste? Spensur losh e izmamno prase</label>
+    </>
+  )
+}
+
+export default Home
